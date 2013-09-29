f99913b853caa66bbe71fc796a3b895f1a96cf18
Sahid Orentino Ferdjaoui <sahid.ferdjaoui@cloudwatt.com>
2013-09-29 15:44:18 +0000

    Uses now env values to identify

--- warm/__init__.py
+++ warm/__init__.py
@@ -6,7 +6,6 @@ import yaml
 
 from openstackclient.common import clientmanager
 from neutronclient.neutron import client as neutron #TODO(sahid): Needs to remove.
-
 from warm import components
 
 DEFAULT_CFGFILE = "config.yaml"
@@ -19,6 +18,11 @@ DEFAULT_OBJECT_API_VERSION = '1'
 DEFAULT_VOLUME_API_VERSION = '1'
 DEFAULT_DOMAIN = 'default'
 DEFAULT_KEY_NAME = 'my'
+
+OS_USERNAME = os.getenv("OS_USERNAME")
+OS_PASSWORD = os.getenv("OS_PASSWORD")
+OS_TENANT_NAME = os.getenv("OS_TENANT_NAME")
+OS_AUTH_URL = os.getenv("OS_AUTH_URL")
 
 class Agent(object):
     def __init__(self, **options):
@@ -40,20 +44,20 @@ class Agent(object):
         self.client = clientmanager.ClientManager(
             token=self.options.get("token"),
             url=self.options.get("url"),
-            auth_url=self.options.get("auth_url"),
-            project_name=self.options.get("project_name"),
+            auth_url=self.options.get("auth_url", OS_AUTH_URL),
+            project_name=self.options.get("project_name", OS_TENANT_NAME),
             project_id=self.options.get("project_id"),
-            username=self.options.get("username"),
-            password=self.options.get("password"),
+            username=self.options.get("username", OS_USERNAME),
+            password=self.options.get("password", OS_PASSWORD),
             region_name=self.options.get("region_name"),
             api_version=self.api_version)
 
         self.clientneutron = neutron.Client(
             token=self.options.get("token"),
-            username=self.options.get("username"),
-            password=self.options.get("password"),
-            tenant_name=self.options.get("project_name"),
-            auth_url=self.options.get("auth_url"),
+            username=self.options.get("username", OS_USERNAME),
+            password=self.options.get("password", OS_PASSWORD),
+            tenant_name=self.options.get("project_name", OS_TENANT_NAME),
+            auth_url=self.options.get("auth_url", OS_AUTH_URL),
             region_name=self.options.get("region_name"),
             insecure=self.options.get("insecure", True),
             api_version=self.options.get("api_version", "2.0"))
